b4f1ee776e9e57a45e0510b54739e70f575bf796
moestilos <guillermo.moestilos@gmail.com>
2024-09-23 09:59:48 +0200

    Esto he hecho en clase

---
+++ clase/ejercicios.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="estilos.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Sofadi+One&display=swap" rel="stylesheet">
+    <title>Ejercicios</title>
+</head>
+<body>
+    
+    <h1>Ejercicios: </h1>
+
+
+    <div>    <?php 
+    $piruletas = 20;
+    $estudiantes = 3;
+    $sobran = $piruletas%$estudiantes;
+    $tocan = ($piruletas - $sobran) / $estudiantes;
+
+    echo "Ejercicio 1: <br> -------------------- <br>
+    Nos sobran ---> $sobran <br> Cada estudiante le toca ---> $tocan <br>
+    --------------------";
+
+ 
+
+
+
+
+    ?></div>
+
+
+
+
+
+
+
+
+
+</body>
+</html>
